99b8e1e5c3c73c9322eab4511cb9ff2661c5887c
Robert Guttersohn <rguttersohn@cccnewyork.org>
2022-08-22 23:36:12 -0400

    added additional ui to tree sidebar content

--- components/sidebar/TreeTab.vue
+++ components/sidebar/TreeTab.vue
@@ -1,7 +1,17 @@
+ <script setup>
+ import { storeToRefs } from 'pinia';
+import { useTreeStore } from '~~/store/trees';
+
+ let treeStore = useTreeStore();
+ let {activeTreeData} = storeToRefs(treeStore);
+ </script>
  <template>
-   <div>
+   <div v-if="Object.keys(activeTreeData).length > 0">
        <SidebarTreeHealth/>
        <SidebarTreeInfo/>
        <SidebarTreeSurroundingArea/>
+   </div>
+   <div v="else">
+    <h3 class="text-center">Click on a tree to display data</h3>
    </div>
 </template>
