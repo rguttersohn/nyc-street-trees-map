6a734650e28d18f5f0d47401d1136b9378d8c44f
Robert Guttersohn <rguttersohn@cccnewyork.org>
2022-09-03 22:51:14 -0400

    started adding icons to tree info component

--- components/sidebar/tree/Properties.vue
+++ components/sidebar/tree/Properties.vue
@@ -2,6 +2,12 @@
 import { storeToRefs } from 'pinia';
 import { useTreeStore } from '~~/store/trees';
 
+// images
+import treeIcon from '~/assets/img/nature_FILL0_wght400_GRAD0_opsz48.svg';
+import apartmentIcon from '~/assets/img/apartment_FILL0_wght400_GRAD0_opsz48.svg';
+
+
+
 let treeStore = useTreeStore();
 
 let {activeTreeData} = storeToRefs(treeStore);
@@ -11,10 +17,12 @@ const specie = computed(() => activeTreeData.value.spc_common ? activeTreeData.v
 
 <template>
     <div class="my-3">
+        <img :src="treeIcon" alt="icon of a tree" class="block mx-auto">
         <h3 class="font-bold">Species:</h3>
         <p>{{ specie }}</p>
     </div>
     <div class="my-3">
+        <img :src="apartmentIcon" alt="icon of a tree" class="block mx-auto">
         <h3 class="font-bold">Address:</h3>
         <p>{{ activeTreeData.address }}</p>
     </div>
